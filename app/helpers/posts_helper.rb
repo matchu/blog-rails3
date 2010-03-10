560d4a8e58f5a477fa17f576026f61ddb285fe6e
Matchu <matchu1993@gmail.com>
2010-03-09 21:38:17 -0500

    swap older and newer - whoops

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def post_pagination
-    will_paginate @posts, :previous_label => '&#8592; Older',
-      :next_label => 'Newer &#8594;'
+    will_paginate @posts, :previous_label => '&#8592; Newer',
+      :next_label => 'Older &#8594;'
   end
 end
